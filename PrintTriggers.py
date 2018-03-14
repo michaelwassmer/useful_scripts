1fddc11567043b4501c4d83ea7c11cb7cfec0e0a
Michael Wassmer <michael.wassmer@cern.ch>
2018-03-14 11:03:08 +0100

    small update

--- PrintTriggers.py
+++ PrintTriggers.py
@@ -1,6 +1,6 @@
 # this script print the trigger paths / filters of a MINIAOD file you give as an argument
 # trigger paths if you use TriggerResults::HLT
-# filters if you use TriggerResults::RECO
+# filters if you use TriggerResults::RECO for data or TriggerResults::PAT for mc
 import ROOT
 import sys
 from DataFormats import FWLite
@@ -9,17 +9,20 @@ ROOT.FWLiteEnabler.enable()
 ROOT.gSystem.Load('libDataFormatsFWLite.so')
 events = FWLite.Events(sys.argv[1])
 triggerResultsHandle = FWLite.Handle('edm::TriggerResults')
-label = 'TriggerResults::HLT'
-#label =	'TriggerResults::RECO'
-i=0
+#label = 'TriggerResults::HLT'
+#label = 'TriggerResults::RECO'
+label =	'TriggerResults::PAT'
+k=0
 for event in events:
-    if i>0:
+    if k>0:
         break
     event.getByLabel(label, triggerResultsHandle)
     triggerResults = triggerResultsHandle.product()
     triggerNames = event.object().triggerNames(triggerResults)
     for i in range(triggerResults.size()):
+        #if not "MET" in triggerNames.triggerName(i):
+            #continue
         print '#{0:03}   {2:5}   {1}'.format(
             i, triggerNames.triggerName(i), str(triggerResults.accept(i))
             )
-    i+=1
+    k+=1
