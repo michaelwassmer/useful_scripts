055401742405b78e6887f2c414c3acb761e96eb6
Michael Wassmer <michael.wassmer@cern.ch>
2018-05-08 16:35:23 +0200

    corrections and improvements

--- V_boson_naf.py
+++ V_boson_naf.py
@@ -5,7 +5,7 @@ import stat
 import os
 file_prefix="root://xrootd-cms.infn.it//"
 
-def get_first_file(dataset_name):
+def get_files(dataset_name):
     print dataset_name
     data=das_client.get_data("file dataset="+dataset_name)
     files=[]
@@ -18,33 +18,39 @@ def get_first_file(dataset_name):
             files.append(file_prefix+f['name'])
     return files
 
-def split_files(files,splitting):
+def split_files_into_jobs(files,events_per_job):
     events=0
     file_splitting = []
-    job = []
+    files_in_job = []
     for i,file in enumerate(files):
-        job.append(file)
+        print "file ",i
+        files_in_job.append(file)
         file_ = ROOT.TFile.Open(file)
         tree = file_.Get("Events")
         events+=tree.GetEntries()
-        if events>=splitting or i==(len(files)-1):
-            file_splitting.append(job)
+        file_.Close()
+        if events>=events_per_job or i==(len(files)-1):
+            file_splitting.append(files_in_job)
             events=0
-            job = []
+            files_in_job = []
     return file_splitting
 
-def print_shell_script(files,postfix):
+def print_shell_script(boson,postfix,files):
     script=""
     script+="#!/bin/bash\n"
     script+="export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch\n"
     script+="source $VO_CMS_SW_DIR/cmsset_default.sh\n"
     script+="cd /nfs/dust/cms/user/mwassmer/DarkMatter/CMSSW_8_0_26_patch2//src\n"
     script+="eval `scram runtime -sh`\n"
+    if not os.path.isdir("root_files"):
+        os.mkdir("root_files")
     script+="cd /nfs/dust/cms/user/mwassmer/DarkMatter/useful_scripts/root_files\n"
-    script+="python /nfs/dust/cms/user/mwassmer/DarkMatter/useful_scripts/V_boson_pt_reweighting.py "+postfix
+    script+="python /nfs/dust/cms/user/mwassmer/DarkMatter/useful_scripts/V_boson_pt_reweighting.py "+boson+" "+postfix
     for file in files:
         script+=" "+file
-    filename = 'scripts/z_boson_pt_'+postfix+'.sh'
+    if not os.path.isdir("scripts"):
+        os.mkdir("scripts")
+    filename = 'scripts/'+boson+'_boson_pt_'+postfix+'.sh'
     f=open(filename,'w')
     f.write(script)
     f.close()
@@ -52,9 +58,12 @@ def print_shell_script(files,postfix):
     st = os.stat(filename)
     os.chmod(filename, st.st_mode | stat.S_IEXEC)
 
-
-files = get_first_file(str(sys.argv[1]).replace('"',''))
-file_splitting = split_files(files,500000)
+boson = str(sys.argv[1])
+if not (boson=="Z" or boson=="W"):
+    print "first argument has to be Z or W"
+    exit()
+files = get_files(str(sys.argv[2]).replace('"',''))
+file_splitting = split_files_into_jobs(files,1)
 print file_splitting
 for i,files in enumerate(file_splitting):
-    print_shell_script(files,str(i))
+    print_shell_script(boson,str(i),files)
--- V_boson_pt_reweighting.py
+++ V_boson_pt_reweighting.py
@@ -4,7 +4,11 @@ from array import array
 from DataFormats.FWLite import Events, Handle
 from math import *
 
-events = Events (sys.argv[2:])
+boson=str(sys.argv[1])
+postfix=str(sys.argv[2])
+filename = sys.argv[3]
+
+events = Events (filename)
 
 handlePruned  = Handle ("std::vector<reco::GenParticle>")
 handlePacked  = Handle ("std::vector<pat::PackedGenParticle>")
@@ -14,8 +18,42 @@ labelPacked = "packedGenParticles"
 labelWeight = "generator"
 
 binning = [30,40,50,60,70,80,90,100,110,120,130,140,150,200,250,300,350,400,450,500,550,600,650,700,750,800,850,900,950,1000,1100,1200,1300,1400,1600,1800,2000,2200,2400,2600,2800,3000,6500]
-v_boson_pt = ROOT.TH1D("z_boson_pt","z_boson_pt",len(binning)-1,array('d',binning))
-file_ = ROOT.TFile("z_boson_pt_"+str(sys.argv[1])+".root","RECREATE")
+v_boson_pt_hist = ROOT.TH1D(boson+"_boson_pt",boson+"_boson_pt",len(binning)-1,array('d',binning))
+file_ = ROOT.TFile(boson+"_boson_pt_"+postfix+".root","RECREATE")
+
+weight_xs = 1.
+if boson=="Z":
+    if "50To100" in filename:
+        weight_xs = 3*0.971*0.0000173748656012
+    elif "100To250" in filename:
+        weight_xs = 3*0.971*0.00000638503945658
+    elif "250To400" in filename:
+        weight_xs = 3*0.971*0.0000184890023189
+    elif "400To650" in filename:
+        weight_xs = 3*0.971*0.000203629482553
+    elif "650ToInf" in filename:
+        weight_xs = 3*0.971*0.000208821846317
+    else:
+        print "problem with xs weight"
+        exit()
+elif boson=="W":
+    if "100To250" in filename:
+        weight_xs = 0.00000385148734634
+    elif "250To400" in filename:
+        weight_xs = 0.0000396029149925
+    elif "400To600" in filename:
+        weight_xs = 0.000265993494557
+    elif "600ToInf" in filename:
+        weight_xs = 0.000255801929068
+    else:
+        print "problem with xs weight"
+        exit()
+else:
+    print "only W or Z boson allowed"
+    exit()
+    
+print "weight_xs = ",weight_xs
+
 # loop over events
 count= 0
 for event in events:
@@ -33,28 +71,36 @@ for event in events:
     weight = eventinfo.product().weight()
     decay_prods = []
     for p in pruned:
-        if p.pdgId()!=23:
-            continue
-        #print "found Z boson"
-        for i in range(p.numberOfDaughters()):
-            daughter = p.daughter(i)
-            if daughter.status()!=1:
-                #print "not stable"
+        if boson=="Z":
+            if p.pdgId()!=23:
                 continue
-            if not (abs(daughter.pdgId())==12 or abs(daughter.pdgId())==14 or abs(daughter.pdgId())==16):
-                #print "no neutrino"
-                continue
-            #print "found neutrino"
-            decay_prods.append(daughter.p4())
+            #print "found Z boson"
+            for i in range(p.numberOfDaughters()):
+                daughter = p.daughter(i)
+                if daughter.status()!=1:
+                    #print "not stable"
+                    continue
+                if not (abs(daughter.pdgId())==12 or abs(daughter.pdgId())==14 or abs(daughter.pdgId())==16):
+                    #print "no neutrino"
+                    continue
+                #print "found neutrino"
+                decay_prods.append(daughter.p4())
+        elif boson=="W":
+            print "not yet implemented"
+            exit()
+        else:
+            print "only W or Z boson allowed"
+            exit()
     if len(decay_prods)!=2:
+        print "v boson could not be determined"
         continue
-    z_boson = decay_prods[0]+decay_prods[1]
-    z_boson_pt = z_boson.pt()
+    v_boson = decay_prods[0]+decay_prods[1]
+    v_boson_pt = v_boson.pt()
     #print z_boson_pt,weight
-    v_boson_pt.Fill(z_boson_pt,weight)
+    v_boson_pt_hist.Fill(v_boson_pt,weight*weight_xs/1000.)
     count+=1
     
-file_.WriteTObject(v_boson_pt)
+file_.WriteTObject(v_boson_pt_hist)
 file_.Close()
     
                 
