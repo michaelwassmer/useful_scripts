d645bc7ade01805537d30ea14d02aac3252dbb8b
Michael Wassmer <michael.wassmer@cern.ch>
2018-06-01 15:31:27 +0200

    add gamma induced contributions to theory histograms, add muR muF variations for mc histograms, add alpha_s variations for theory

--- CalculateCorrectionsAndUncertainties.py
+++ CalculateCorrectionsAndUncertainties.py
@@ -34,7 +34,7 @@ def K_TH(QCD_ORDER,EW_ORDER,e_QCD=[],e_EW=[],e_MIX = 0.):
 
 def sigma_TH(QCD_ORDER,EW_ORDER,e_QCD=[],e_EW=[],e_MIX = 0.):
     sigma_QCD_LO = file_TH.Get(process+"_pTV_"+"LO").Clone()
-    sigma_TH = K_TH(QCD_ORDER,EW_ORDER,e_QCD,e_EW,e_MIX)*sigma_QCD_LO
+    sigma_TH = (K_TH(QCD_ORDER,EW_ORDER,e_QCD,e_EW,e_MIX)*sigma_QCD_LO)+file_TH.Get(process+"_pTV_gammaind_LO").Clone()
     str_QCD = ""
     str_EW = ""
     str_MIX = ""
@@ -53,7 +53,7 @@ def sigma_TH(QCD_ORDER,EW_ORDER,e_QCD=[],e_EW=[],e_MIX = 0.):
 output = ROOT.TFile("TheoryXS_"+process+".root","RECREATE")
 
 
-test = sigma_TH("NNLO","NLO",[0.,0.,0.],[0.,0.,0.],0.)
+nom = sigma_TH("NNLO","NLO",[0.,0.,0.],[0.,0.,0.],0.)
 
 output_hists = [sigma_TH("NNLO","NLO",[e_QCD1,e_QCD2,e_QCD3],[e_EW1,e_EW2,e_EW3],e_MIX) for e_QCD1 in [1,0,-1] for e_QCD2 in [1,0,-1] for e_QCD3 in [1,0,-1] for e_EW1 in [1,0,-1] for e_EW2 in [1,0,-1] for e_EW3 in [1,0,-1] for e_MIX in [1,0,-1]]
 print len(output_hists)
@@ -74,5 +74,36 @@ else:
 for hist in output_hists:
     hist.Divide(hist_mc)
     output.WriteTObject(hist)
+
+# muR muF variations of mc sample
+for scale in ["Weight_scale_variation_muR_0p5_muF_1p0","Weight_scale_variation_muR_2p0_muF_1p0","Weight_scale_variation_muR_1p0_muF_0p5","Weight_scale_variation_muR_1p0_muF_2p0"]:
+    nom_clone = nom.Clone()
+    nom_clone.SetName(nom_clone.GetName()+"_"+scale)
+    nom_clone.SetTitle(nom_clone.GetTitle()+"_"+scale)
+    V=None
+    if process=="vvj":
+        V="Z"
+    elif process=="evj":
+        V="W"
+    else:
+        print "wrong option"
+        exit()
+    nom_clone.Divide(file_mc.Get(V+"_boson_pt_"+scale).Clone())
+    output.WriteTObject(nom_clone)
+
+# error of alpha_s for theory histograms
+alpha_up = nom.Clone()
+alpha_up.SetName(alpha_up.GetName()+"_alpha_up")
+alpha_up.SetTitle(alpha_up.GetTitle()+"_alpha_up")
+alpha_down = nom.Clone()
+alpha_down.SetName(alpha_down.GetName()+"_alpha_down")
+alpha_down.SetTitle(alpha_down.GetTitle()+"_alpha_down")
+for i in range(alpha_up.GetNbinsX()):
+    alpha_up.SetBinContent(i,alpha_up.GetBinContent(i)+alpha_up.GetBinError(i))
+    alpha_down.SetBinContent(i,alpha_down.GetBinContent(i)-alpha_down.GetBinError(i))
+alpha_up.Divide(hist_mc)
+alpha_down.Divide(hist_mc)
+output.WriteTObject(alpha_up)
+output.WriteTObject(alpha_down)
 
 output.Close()
