109f70e412b25950d7cf86645cb354d9817ff1f5
Michael Wassmer <michael.wassmer@cern.ch>
2020-03-24 18:00:20 +0100

    save useful command

--- cross_section_calculation/commands.sh
+++ cross_section_calculation/commands.sh
@@ -2,3 +2,9 @@
 for i in $(cat datasets.txt); do dasgoclient --query="file dataset=$i" | head -n 30 > $(echo $i | cut -d"/" -f2 | cut -d"/" -f1)_FILES.txt;done
 # command to run the cross section calculation over the files and print the result to a text file
 for i in *FILES.txt; do cmsRun ana.py maxEvents=1000000 inputFiles_load=$i 2>&1 | tee ${i%_FILES.txt}_XS.txt;done
+# command to read cross section from file
+for i in *XS.txt
+do
+  echo ${i%_XS.txt}
+  grep "After filter: final cross section" ${i}
+done
