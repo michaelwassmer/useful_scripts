e4babc57f1d3c4af031d65b24f71881f803289c2
Michael Wassmer <michael.wassmer@cern.ch>
2020-07-17 09:44:25 +0200

    small correction for cross section calculation shell command to use all files

--- cross_section_calculation/commands.sh
+++ cross_section_calculation/commands.sh
@@ -1,5 +1,5 @@
 # command to write the files for several datasets which are in a text file called datasets.txt using the dasgoclient
-for i in $(cat datasets.txt); do dasgoclient --query="file dataset=$i" | head -n 30 > $(echo $i | cut -d"/" -f2 | cut -d"/" -f1)_FILES.txt;done
+for i in $(cat datasets.txt); do dasgoclient --query="file dataset=$i" | cat > $(echo $i | cut -d"/" -f2 | cut -d"/" -f1)_FILES.txt;done
 # command to run the cross section calculation over the files and print the result to a text file
 for i in *FILES.txt; do cmsRun ana.py maxEvents=1000000 inputFiles_load=$i 2>&1 | tee ${i%_FILES.txt}_XS.txt;done
 # command to read cross section from file
