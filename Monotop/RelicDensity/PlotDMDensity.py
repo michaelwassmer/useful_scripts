bfc7d932661374356ecf99ea1b12e03dfc9e3449
Michael Wassmer <michael.wassmer@cern.ch>
2023-02-20 13:23:20 +0100

    save

--- Monotop/RelicDensity/PlotDMDensity.py
+++ Monotop/RelicDensity/PlotDMDensity.py
@@ -6,6 +6,13 @@ ROOT.gROOT.SetBatch(True)
 
 file = sys.argv[1]
 
+xtitle="Mediator mass m_{V}(GeV)"
+ytitle="DM mass m_{#chi}(GeV)"
+if len(sys.argv)>=3:
+    xtitle = str(sys.argv[2])
+if len(sys.argv)>=4:
+    ytitle = str(sys.argv[3])
+
 med_mass = []
 dm_mass = []
 relic_density = []
@@ -13,13 +20,21 @@ relic_density = []
 with open(file) as infile:
     for line in infile.readlines():
         line_as_list=line.split("\t")
+        if float(line_as_list[2])<0.:
+            continue
         med_mass.append(float(line_as_list[1]))
         dm_mass.append(float(line_as_list[2]))
         relic_density.append(float(line_as_list[3]))
 
-
 graph=ROOT.TGraph2D(len(med_mass),array("f",med_mass),array("f",dm_mass),array("f",relic_density))
-graph.SetTitle("Relic density as a function of model parameters;Mediator mass (GeV);DM mass (GeV);Relic density")
+graph.SetTitle("Relic density as a function of model parameters;{};{};Relic density".format(xtitle,ytitle))
+
+max = graph.GetHistogram().GetMaximum()
+min = graph.GetHistogram().GetMinimum()
+graph.GetHistogram().GetZaxis().SetRangeUser(min*0.1,max*10)
+#graph.GetHistogram().GetZaxis().SetRangeUser(0.001,100)
+#graph.GetHistogram().GetYaxis().SetRangeUser(0.,1250)
+#graph.GetHistogram().GetXaxis().SetRangeUser(0.,2500)
 
 graph_contour=graph.Clone()
 contours = array('d',[0.1186,0.1186+0.002,0.1186-0.002])
@@ -30,7 +45,7 @@ graph_contour.SetLineColor(ROOT.kBlack)
 
 c=ROOT.TCanvas()
 
-leg=ROOT.TLegend(0.15,0.75,0.45,0.83)
+leg=ROOT.TLegend(0.15,0.76,0.45,0.84)
 leg.SetMargin(0.15)
 leg.AddEntry(graph_contour,"#Omega_{nbm}h^{2}=0.1186#pm 0.0020","l")
 
@@ -41,4 +56,4 @@ ROOT.gPad.SetRightMargin(0.13)
 graph_contour.Draw("cont3 same")
 leg.Draw("same")
 
-c.Print("relic_density.pdf")
+c.Print("relic_density_{}.pdf".format(file.replace(".txt","")))
